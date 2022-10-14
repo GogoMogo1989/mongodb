1e8ea5e0cd18ad71f0d5f3f33ad0e17d93c9e5d6
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-10-14 15:42:28 +0200

    20221014

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection
+let uri ="mongodb+srv://GogoMogo1989:Bp19891207@cluster0.0x4xav2.mongodb.net/?retryWrites=true&w=majority"
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://127.0.0.1:27017/bookstore')
+    MongoClient.connect(uri)
       .then(client => {
         dbConnection = client.db()
         return cb()
